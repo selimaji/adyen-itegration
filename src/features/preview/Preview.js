edabe4da6ccb457f1ae9e4fb24f31418039ee8ec
izet <selimajizet21@gmail.com>
2024-08-25 17:37:54 +0200

    WIP

--- src/features/preview/Preview.js
+++ src/features/preview/Preview.js
@@ -1,35 +1,30 @@
 import React from "react";
-import { Link, useParams } from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 
 export const PreviewContainer = () => {
-  let { type } = useParams();
+    let {type} = useParams();
 
-  return (
-    <main className="preview-page">
-      <section className="cart">
-        <h2>Cart</h2>
-        <div className="order-summary">
-          <ul className="order-summary-list">
-            <li className="order-summary-list-list-item">
-              <img src="/images/sunglasses.png" className="order-summary-list-list-item-image" alt="" />
-              <p className="order-summary-list-list-item-title">Sunglasses</p>
-              <p className="order-summary-list-list-item-price">50.00</p>
-            </li>
-            <li className="order-summary-list-list-item">
-              <img src="/images/headphones.png" className="order-summary-list-list-item-image" alt="" />
-              <p className="order-summary-list-list-item-title">Headphones</p>
-              <p className="order-summary-list-list-item-price">50.00</p>
-            </li>
-          </ul>
-        </div>
-        <div className="cart-footer">
-          <span className="cart-footer-label">Total:</span>
-          <span className="cart-footer-amount">100.00</span>
-          <Link to={`/checkout/${type}`}>
-            <p className="button">Continue to checkout</p>
-          </Link>
-        </div>
-      </section>
-    </main>
-  );
+    return (
+        <main className="preview-page">
+            <section className="cart">
+                <h2>Cart</h2>
+                <div className="order-summary">
+                    <ul className="order-summary-list">
+                        <li className="order-summary-list-list-item">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwhBuujtnSRXUkYD6OuRkAkkdZnX3o8tY6FQ&s" className="order-summary-list-list-item-image" alt=""/>
+                            <p className="order-summary-list-list-item-title">Vodafone</p>
+                            <p className="order-summary-list-list-item-price">10.00</p>
+                        </li>
+                    </ul>
+                </div>
+                <div className="cart-footer">
+                    <span className="cart-footer-label">Total:</span>
+                    <span className="cart-footer-amount">10.00</span>
+                    <Link to={`/checkout/${type}`}>
+                        <p className="button">Continue to checkout</p>
+                    </Link>
+                </div>
+            </section>
+        </main>
+    );
 }
